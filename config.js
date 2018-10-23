b9a5dd0b84ea45b5d743c28d1a03e069d9e1b07d
Jomar <jdvista96@gmail.com>
2018-10-23 15:41:15 +0800

    change config

--- config.js
+++ config.js
@@ -12,10 +12,10 @@ const config = {
   },
   production: {
     db: {
-      database: ddb60qah8cpn5n,
-      user: wyofmmwppviiga,
-      password: 615c5ecb4e93d029261948ca99c6d166f165bfa548bd39ad0e0751aaf79df8e6,
-      host: ec2-54-221-225-11.compute-1.amazonaws.com,
+      database: 'ddb60qah8cpn5n',
+      user: 'wyofmmwppviiga',
+      password: '615c5ecb4e93d029261948ca99c6d166f165bfa548bd39ad0e0751aaf79df8e6',
+      host: 'ec2-54-221-225-11.compute-1.amazonaws.com',
       port: 5432,
       ssl: true
     },
